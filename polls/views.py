76741ec4c0835cc13429946917c66afbe8d987a1
Neeji <nagg844@gmail.com>
2019-08-03 12:59:15 +0530

    generic view implemented

--- polls/views.py
+++ polls/views.py
@@ -1,30 +1,25 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Question
+from .models import Question,Choice
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:]
-	return render(request,'polls/index.html',{'latest_question_list':latest_question_list})
-	# output = ', '.join([q.question_text for q in latest_question_list])
-	# return HttpResponse(output)
-	# return HttpResponse("hello world, This is my first django app.")
-	
-def detail(request, question_id):
-    # return HttpResponse("You're looking at question %s." % question_id)
-    # can comment these out as there is a shortcut to to the below try catch in teo lines
-    # try:
-    # 	question = Question.objects.get(pk=question_id)
-    # except:
-    # 	raise Http404("Question with provided id doesnot exits.")
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/details.html',{'question':question})
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
 
-def results(request, question_id):
-    # response = "You're looking at the results of question %s."
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/results.html',{'question':question})
-    # return HttpResponse(response % question_id)
+	def get_queryset(self):
+		""" Return the last five published questions. """
+		return Question.objects.order_by('-pub_date')[:10]
+
+class DetailView(generic.DetailView):
+	model=Question
+	template_name = 'polls/details.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     # return HttpResponse("You're voting on question %s." % question_id)
